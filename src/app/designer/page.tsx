45177e0e7b2bf145995524ca8a4a0d5a4d05d356
Brian Sinclair <briansinclair@Brians-iMac.local>
2026-03-20 09:59:57 +0000

    Fix designer page: mount DecoratorStudio instead of redirect

--- src/app/designer/page.tsx
+++ src/app/designer/page.tsx
@@ -1,6 +1,20 @@
-import { redirect } from "next/navigation";
+import { AppShell } from "@/components/app-shell";
+import { shellCopy } from "@/lib/content";
+import { listDecoratorProducts, listDecoratorTemplates } from "@/lib/data-repository";
+import { DecoratorStudio } from "@/components/decorator/decorator-studio";
 
-export default function LegacyDesignerRoute() {
-  redirect("/artwork-templates");
+export const dynamic = "force-dynamic";
+
+export default async function DesignerPage() {
+  const [products, templates] = await Promise.all([
+    listDecoratorProducts(),
+    listDecoratorTemplates(),
+  ]);
+
+  return (
+    <AppShell title={shellCopy.designer.title}>
+      <DecoratorStudio products={products} templates={templates} />
+    </AppShell>
+  );
 }
 
